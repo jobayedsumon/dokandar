59d7e617f175956aa5e5d58e5dd5799087496d4d
Jobayed Sumon <jobayedahmedsumon@gmail.com>
2021-03-03 01:51:01 +0600

    Working on fb, google

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Socialite;
+use Laravel\Socialite\Facades\Socialite;
 use App\User;
 use App\Customer;
 use Illuminate\Http\Request;
