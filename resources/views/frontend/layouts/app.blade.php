280c61d876fd43e6a4fc3c7221eb34379b6dc389
Jobayed Sumon <jobayedahmedsumon@gmail.com>
2021-03-03 01:12:23 +0600

    Working on fb

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -125,6 +125,29 @@
 <body>
 
 
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '2779828275605262',
+            cookie     : true,
+            xfbml      : true,
+            version    : 'v10.0'
+        });
+
+        FB.AppEvents.logPageView();
+
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+
+
 <!-- MAIN WRAPPER -->
 <div class="body-wrap shop-default shop-cards shop-tech gry-bg">
 
