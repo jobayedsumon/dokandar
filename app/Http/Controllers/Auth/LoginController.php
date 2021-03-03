43647ee56485f819e992b0cdaa400fb0a435fef3
Jobayed Sumon <jobayedahmedsumon@gmail.com>
2021-03-03 17:50:39 +0000

    local

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -58,6 +58,7 @@ class LoginController extends Controller
                 $user = Socialite::driver($provider)->stateless()->user();
             }
         } catch (\Exception $e) {
+            dd($e->getMessage());
             flash("Something Went wrong. Please try again.")->error();
             return redirect()->route('user.login');
         }
